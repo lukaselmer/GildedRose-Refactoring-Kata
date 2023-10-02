9f1ab0984f664d0f060aff9a4f9a9e5a973e5881
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-03 00:10:53 +0200

    One commit per save

--- TypeScript/src/NormalItem.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from './items/Item'
-
-export class NormalItem extends Item {
-  updateQuality() {
-    this.decreaseSellIn()
-    this.decreaseQualityToMin0(this.expired ? 2 : 1)
-  }
-}
