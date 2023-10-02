7200df465fcfc5999da13304b7073b9363fdcdb5
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-03 00:12:11 +0200

    One commit per save

--- TypeScript/src/items/ConjuredItem.ts
+++ TypeScript/src/items/ConjuredItem.ts
@@ -3,6 +3,6 @@ import { Item } from './Item'
 export class ConjuredItem extends Item {
   updateQuality() {
     this.decreaseSellIn()
-    this.decreaseQualityToMin0(this.expired ? 2 : 1)
+    this.decreaseQualityToMin0(this.expired ? 4 : 2)
   }
 }
