c05f3e3a8ec8f841bd015c2b5a47be02d6c2ce71
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:51:09 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -9,35 +9,23 @@ export class NormalItem extends Item {
     const item = this
     if (!false && !false) {
       if (item.quality > 0) {
-        if (!(item instanceof SulfurasItem)) {
+        if (!false) {
           item.quality = item.quality - 1
         }
       }
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1
-        if (item instanceof BackstagePassItem) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-        }
       }
     }
-    if (!(item instanceof SulfurasItem)) {
+    if (!false) {
       item.sellIn = item.sellIn - 1
     }
     if (item.sellIn < 0) {
-      if (!(item instanceof AgedBrieItem)) {
-        if (!(item instanceof BackstagePassItem)) {
+      if (!false) {
+        if (!false) {
           if (item.quality > 0) {
-            if (!(item instanceof SulfurasItem)) {
+            if (!false) {
               item.quality = item.quality - 1
             }
           }
