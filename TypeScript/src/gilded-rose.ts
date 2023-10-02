29aeb61e860f8205c56879f57affc1203e58dbbb
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:50:51 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -7,7 +7,7 @@ abstract class Item {
 export class NormalItem extends Item {
   updateQuality() {
     const item = this
-    if (!(item instanceof AgedBrieItem) && !(item instanceof BackstagePassItem)) {
+    if (!false && !(item instanceof BackstagePassItem)) {
       if (item.quality > 0) {
         if (!(item instanceof SulfurasItem)) {
           item.quality = item.quality - 1
