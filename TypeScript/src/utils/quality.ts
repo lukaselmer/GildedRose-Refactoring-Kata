fec21d4d325ed4dd11e07b61975c3d89d72ad120
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 22:46:55 +0200

    One commit per save

--- TypeScript/src/utils/quality.ts
+++ TypeScript/src/utils/quality.ts
@@ -1,11 +1,11 @@
 import { Item } from '../Item'
 
-export function decreaseQualityToMin0(item: Item, by = 1) {
-  item.quality = Math.max(0, item.quality - by)
-}
-
 export function expireBackstagePass(item: Item) {
   item.quality = 0
+}
+
+export function decreaseQualityToMin0(item: Item, by = 1) {
+  item.quality = Math.max(0, item.quality - by)
 }
 
 export function increaseQualityToMax50(item: Item, by = 1) {
