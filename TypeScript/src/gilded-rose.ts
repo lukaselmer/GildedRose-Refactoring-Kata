6b1884219f7d6d51f07e4d3f645993a5e37a73d4
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-03 00:05:29 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -52,7 +52,7 @@ export class AgedBrieItem extends Item {
 
 export class BackstagePassItem extends Item {
   updateQuality() {
-    if (this.expiringVerySoon) this.increaseQualityToMax50()
+    if (this.expiringVerySoon) this.increaseQualityToMax50(1)
     if (this.expiringSoon) this.increaseQualityToMax50()
     this.increaseQualityToMax50()
 
