825dd5aecaf4cd150a80c7497b6dacfd3611a303
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:56:56 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -29,7 +29,6 @@ export class BackstagePassItem extends Item {
     if (item.quality < 50) {
       item.quality = item.quality + 1
       if (item.sellIn < 11 && item.quality < 50) item.quality = item.quality + 1
-
       if (item.sellIn < 6 && item.quality < 50) item.quality = item.quality + 1
     }
     item.sellIn = item.sellIn - 1
