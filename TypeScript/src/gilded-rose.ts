0e7b0cb77f05d63805a2dcd81b086f79b87e741e
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 22:15:20 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -25,8 +25,7 @@ function updateItemQuality(item: Item) {
         if (item.sellIn < 11 && item.quality < 50) {
           increaseQuality(item)
         }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
+        if (item.sellIn < 6 && item.quality < 50) {
             increaseQuality(item)
           }
         }
