3211fc477d3ae325591af42ea11f19d8a9046553
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:56:07 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -44,9 +44,7 @@ export class BackstagePassItem extends Item {
         }
       }
     }
-    if (true) {
-      item.sellIn = item.sellIn - 1
-    }
+    item.sellIn = item.sellIn - 1
     if (item.sellIn < 0) item.quality = item.quality - item.quality
   }
 }
