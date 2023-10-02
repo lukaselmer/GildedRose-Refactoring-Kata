f5eebc3593a7851e9ae219c81d05f63167e7d3ba
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 22:14:15 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -13,47 +13,51 @@ export class GildedRose {
 
 function updateQualityFn(items: Item[]) {
   for (let i = 0; i < items.length; i++) {
-    const item = items[i]
+    updateItemQuality(items, i)
+  }
+}
 
-    if (normal(item) && item.quality > 0) decreaseQuality(item)
+function updateItemQuality(items: Item[], i: number) {
+  const item = items[i]
 
-    if (agedBrie(item) || backstagePass(item)) {
-      if (item.quality < 50) {
-        increaseQuality(item)
-        if (backstagePass(item)) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              increaseQuality(item)
-            }
+  if (normal(item) && item.quality > 0) decreaseQuality(item)
+
+  if (agedBrie(item) || backstagePass(item)) {
+    if (item.quality < 50) {
+      increaseQuality(item)
+      if (backstagePass(item)) {
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            increaseQuality(item)
           }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              increaseQuality(item)
-            }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            increaseQuality(item)
           }
         }
       }
     }
+  }
 
-    if (!legendary(item)) {
-      item.sellIn = item.sellIn - 1
-    }
+  if (!legendary(item)) {
+    item.sellIn = item.sellIn - 1
+  }
 
-    if (item.sellIn < 0) {
-      if (!agedBrie(item)) {
-        if (!backstagePass(item)) {
-          if (item.quality > 0) {
-            if (!legendary(item)) {
-              decreaseQuality(item)
-            }
+  if (item.sellIn < 0) {
+    if (!agedBrie(item)) {
+      if (!backstagePass(item)) {
+        if (item.quality > 0) {
+          if (!legendary(item)) {
+            decreaseQuality(item)
           }
-        } else {
-          item.quality = item.quality - item.quality
         }
       } else {
-        if (item.quality < 50) {
-          increaseQuality(item)
-        }
+        item.quality = item.quality - item.quality
+      }
+    } else {
+      if (item.quality < 50) {
+        increaseQuality(item)
       }
     }
   }
