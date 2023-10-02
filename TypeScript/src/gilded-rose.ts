a71cecc823fe0a1e00a0ec490153da99e2d22393
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:54:08 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -8,7 +8,6 @@ export class NormalItem extends Item {
   updateQuality() {
     const item = this
     if (item.quality > 0) item.quality = item.quality - 1
-
     item.sellIn = item.sellIn - 1
     if (item.sellIn < 0 && item.quality > 0) item.quality = item.quality - 1
   }
