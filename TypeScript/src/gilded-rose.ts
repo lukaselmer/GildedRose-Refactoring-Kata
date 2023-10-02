d9edc78b38058f2d8bda340bfe22ad51ecf76862
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:50:09 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -5,32 +5,8 @@ abstract class Item {
 }
 
 export class NormalItem extends Item {
-  updateQuality() {}
-}
-
-export class AgedBrieItem extends Item {
-  updateQuality() {}
-}
-
-export class BackstagePassItem extends Item {
-  updateQuality() {}
-}
-
-export class SulfurasItem extends Item {
-  updateQuality() {}
-}
-
-export class GildedRose {
-  constructor(private items: Item[] = []) {}
-
   updateQuality() {
-    this.items.forEach((item) => this.updateItemQuality(item))
-
-    return this.items
-  }
-
-  private updateItemQuality(item: Item) {
-    item.updateQuality()
+    const item = this
     if (!(item instanceof AgedBrieItem) && !(item instanceof BackstagePassItem)) {
       if (item.quality > 0) {
         if (!(item instanceof SulfurasItem)) {
@@ -74,5 +50,166 @@ export class GildedRose {
         }
       }
     }
+  }
+}
+
+export class AgedBrieItem extends Item {
+  updateQuality() {
+    const item = this
+    if (!(item instanceof AgedBrieItem) && !(item instanceof BackstagePassItem)) {
+      if (item.quality > 0) {
+        if (!(item instanceof SulfurasItem)) {
+          item.quality = item.quality - 1
+        }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+        if (item instanceof BackstagePassItem) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (!(item instanceof SulfurasItem)) {
+      item.sellIn = item.sellIn - 1
+    }
+    if (item.sellIn < 0) {
+      if (!(item instanceof AgedBrieItem)) {
+        if (!(item instanceof BackstagePassItem)) {
+          if (item.quality > 0) {
+            if (!(item instanceof SulfurasItem)) {
+              item.quality = item.quality - 1
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      }
+    }
+  }
+}
+
+export class BackstagePassItem extends Item {
+  updateQuality() {
+    const item = this
+    if (!(item instanceof AgedBrieItem) && !(item instanceof BackstagePassItem)) {
+      if (item.quality > 0) {
+        if (!(item instanceof SulfurasItem)) {
+          item.quality = item.quality - 1
+        }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+        if (item instanceof BackstagePassItem) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (!(item instanceof SulfurasItem)) {
+      item.sellIn = item.sellIn - 1
+    }
+    if (item.sellIn < 0) {
+      if (!(item instanceof AgedBrieItem)) {
+        if (!(item instanceof BackstagePassItem)) {
+          if (item.quality > 0) {
+            if (!(item instanceof SulfurasItem)) {
+              item.quality = item.quality - 1
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      }
+    }
+  }
+}
+
+export class SulfurasItem extends Item {
+  updateQuality() {
+    const item = this
+    if (!(item instanceof AgedBrieItem) && !(item instanceof BackstagePassItem)) {
+      if (item.quality > 0) {
+        if (!(item instanceof SulfurasItem)) {
+          item.quality = item.quality - 1
+        }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+        if (item instanceof BackstagePassItem) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (!(item instanceof SulfurasItem)) {
+      item.sellIn = item.sellIn - 1
+    }
+    if (item.sellIn < 0) {
+      if (!(item instanceof AgedBrieItem)) {
+        if (!(item instanceof BackstagePassItem)) {
+          if (item.quality > 0) {
+            if (!(item instanceof SulfurasItem)) {
+              item.quality = item.quality - 1
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      }
+    }
+  }
+}
+
+export class GildedRose {
+  constructor(private items: Item[] = []) {}
+
+  updateQuality() {
+    this.items.forEach((item) => this.updateItemQuality(item))
+
+    return this.items
+  }
+
+  private updateItemQuality(item: Item) {
+    item.updateQuality()
   }
 }
