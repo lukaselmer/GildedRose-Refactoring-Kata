45098bdeabd0e36b8e6b330f0f28285e888aa331
Lukas Elmer <lukas.elmer@gmail.com>
2023-10-02 23:55:05 +0200

    One commit per save

--- TypeScript/src/gilded-rose.ts
+++ TypeScript/src/gilded-rose.ts
@@ -16,13 +16,10 @@ export class AgedBrieItem extends Item {
   updateQuality() {
     const item = this
 
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-    }
+    if (item.quality < 50) item.quality = item.quality + 1
+
     item.sellIn = item.sellIn - 1
-    if (item.sellIn < 0 && item.quality < 50) {
-      item.quality = item.quality + 1
-    }
+    if (item.sellIn < 0 && item.quality < 50) item.quality = item.quality + 1
   }
 }
 
